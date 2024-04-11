d9876c0f136c2a37a4c95dd4cb57e9423c559eb3
Timothy <oludefiyinfoluwa06@gmail.com>
2024-04-11 15:55:07 +0100

    Simple GSAP animation added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { useGSAP } from '@gsap/react';
 import Button from './Button';
 import { FaBars } from 'react-icons/fa'
+import gsap from 'gsap';
 
 const Navbar = () => {
     const [showNav, setShowNav] = useState(false);
@@ -10,34 +12,61 @@ const Navbar = () => {
         setShowNav(prev => !prev);
     }
 
+    useGSAP(() => {
+        gsap.from('.logo', {
+            duration: 1,
+            opacity: 0,
+            y: '-100%'
+        });
+
+        gsap.from('.nav-link', {
+            duration: 1,
+            opacity: 0,
+            y: '-100%',
+            stagger: 0.3
+        });
+
+        gsap.from('#btn', {
+            duration: 1,
+            opacity: 0,
+            y: '-100%',
+            stagger: 0.3
+        });
+
+    }, []);
+
     return (
         <nav className='flex items-center justify-between gap-2'>
-            <label className='text-darkBlue text-[30px] font-bold'>LOGO</label>
+            <label className='text-darkBlue text-[30px] font-bold logo'>LOGO</label>
             <div className='md:flex hidden items-center justify-end gap-10'>
                 <ul className='md:flex items-center justify-center gap-8'>
-                    <li>
+                    <li className='nav-link'>
                         <NavLink className='hover:text-normalBlue' to='/'>Home</NavLink>
                     </li>
-                    <li>
+                    <li className='nav-link'>
                         <NavLink className='hover:text-normalBlue' to='/categories'>Categories</NavLink>
                     </li>
-                    <li>
+                    <li className='nav-link'>
                         <NavLink className='hover:text-normalBlue' to='/about'>About</NavLink>
                     </li>
-                    <li>
+                    <li className='nav-link'>
                         <NavLink className='hover:text-normalBlue' to='/jobs'>Jobs</NavLink>
                     </li>
-                    <li>
+                    <li className='nav-link'>
                         <NavLink className='hover:text-normalBlue' to='/recruiters'>Recruiters</NavLink>
                     </li>
                 </ul>
                 <div className='flex items-center justify-end gap-2'>
-                    <Link to='/signup'>
-                        <Button text='Register' bg={false} />
-                    </Link>
-                    <Link to='/login'>
-                        <Button text='Login' bg={true} />
-                    </Link>
+                    <div id='btn'>
+                        <Link to='/signup'>
+                            <Button text='Register' bg={false} />
+                        </Link>
+                    </div>
+                    <div id='btn'>
+                        <Link to='/login'>
+                            <Button text='Login' bg={true} />
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div className={`flex flex-col md:hidden absolute top-[70px] ${showNav ? 'right-[20px]' : 'right-[-100%]'} z-10 bg-white shadow-lg p-4 border w-[300px] rounded-md gap-4 transition-all duration-300 ease-in-out`}>
@@ -67,7 +96,7 @@ const Navbar = () => {
                     </Link>
                 </div>
             </div>
-            <div className='text-black text-[23px] cursor-pointer md:hidden' onClick={handleShowNav}>
+            <div className='text-black text-[23px] cursor-pointer md:hidden' id='btn' onClick={handleShowNav}>
                 <FaBars />
             </div>
         </nav>
