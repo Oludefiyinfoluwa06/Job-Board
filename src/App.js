36376b9cc0419fb0b3cda028cfcbb00820a78764
Icaalex <ndukwuuju6@gmail.com>
2024-04-12 06:47:10 -0700

    a new branch

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-red-800">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-red-900"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
