85558d2fb5701dd5eb4e0e0a92f3bff138b2f148
Timothy <oludefiyinfoluwa06@gmail.com>
2024-04-15 13:06:49 +0100

    Footer updated with animation

--- client/src/components/landing_page/Footer.jsx
+++ client/src/components/landing_page/Footer.jsx
@@ -1,14 +1,31 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const Footer = () => {
+    useGSAP(() => {
+        gsap.from('#footer', {
+            scrollTrigger: {
+                trigger: '#footer',
+            },
+            duration: 1,
+            opacity: 0,
+            yPercent: 100,
+            stagger: 0.3
+        })
+    }, []);
+
     return (
-        <footer className='flex items-center justify-between text-[14px] py-5'>
-            <p>Copyright &copy; {new Date().getFullYear()}, All Rights Reserved.</p>
+        <footer className='flex items-center justify-between text-[14px] py-5 border-t-2'>
+            <p id="footer">Copyright &copy; {new Date().getFullYear()}, All Rights Reserved.</p>
             <div className='flex items-center justify-end gap-3'>
-                <Link to='/'>Privacy Policy</Link>
-                <Link to='/'>Terms &amp; condition</Link>
-                <Link to='/'>Security</Link>
+                <Link id="footer" to='/'>Privacy Policy</Link>
+                <Link id="footer" to='/'>Terms &amp; condition</Link>
+                <Link id="footer" to='/'>Security</Link>
             </div>
         </footer>
     );
